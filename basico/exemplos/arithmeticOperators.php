e8cf65dfd114d6ed8a22a1076a9531b431bc07f6
van-gomes <nninha.gomes@gmail.com>
2024-08-12 20:34:47 -0300

    Study operators

---
+++ basico/exemplos/arithmeticOperators.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 10; //a = 10
+$b = 20; //b = 20
+$soma = $a + $b; //Realizando a soma com sinal de + das variáveis $a e $b
+$subtracao = $a - $b; //Realizando a subtração com sinal de - das variáveis $a e $b
+$multiplicacao = $a * $b; //Realizando a multiplicação com sinal de * das variáveis $a e $b
+$divisao = $a % 2; //Realizando a divisão com sinal de % das variáveis $a dividida por 2
+echo $soma . "\n"; // 30
+echo $subtracao . "\n"; // -10
+echo $multiplicacao . "\n"; // 200
+echo $divisao . "\n"; // 5 => resto = 0 (número par)
+
+?>
---
+++ basico/exemplos/assignmentOperators.php
@@ -0,0 +1,17 @@
+<?php
+
+$resultado = 9;
+
+$resultado +=1;
+echo $resultado . "\n"; // 10
+
+$resultado -=5;
+echo $resultado . "\n"; // 8
+
+$resultado *=2;
+echo $resultado . "\n"; // 18
+
+$resultado /=3;
+echo $resultado . "\n"; // 3
+
+?>
