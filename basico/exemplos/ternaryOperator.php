e1bf52d794bef5f81bf8e944f6755bc8e7315db0
van-gomes <nninha.gomes@gmail.com>
2024-08-14 22:18:03 -0300

    Study ternary operator

---
+++ basico/exemplos/ternaryOperator.php
@@ -0,0 +1,14 @@
+<?php
+
+	$nickname = 'danielhe4rt';
+
+	$who = $nickname == "jorge123" ? "é o jorge online" : "não é o jorge online";
+
+	echo $who; //  não é o jorge online
+
+	$modoTeste = true;
+
+	return $modoTeste  ? "MODO DESENVOLVIMENTO ATIVADO" : "MODO DESENVOLVIMENTO DESATIVADO";
+	// return MODO DESENVOLVIMENTO ATIVADO
+
+?>
