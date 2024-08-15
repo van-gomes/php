c771cf2b721f8b09742e3af51e20b0d3cc5fedf0
van-gomes <nninha.gomes@gmail.com>
2024-08-14 22:12:56 -0300

    Study switch case conditional structure

---
+++ basico/exemplos/switchCaseStructure.php
@@ -0,0 +1,45 @@
+<?php
+
+	$comando = "!he4rt";
+
+	switch($comando){
+	    case "!site":
+	        echo "Link: https://heartdevs.com";
+	        echo "Esse é o site oficial";
+	        break;
+	    case "!he4rt":
+	        echo "Entre no nosso discord: https://discord.com/he4rt";
+	        echo "Esse é o discord oficial";
+	        break;
+	    case "!comandos":
+	        echo "Segue a lista de comandos";
+	        echo "!he4rt !site";
+	        break;
+	    default:
+	        echo "nada acontece feijoada";
+	        break;
+	}
+	// return Entre no nosso discord: https://discord.com/he4rt Esse é o discord oficial
+
+	$comando = "!teste";
+
+	switch($comando){
+	    case "!site":
+	        echo "Link: https://heartdevs.com";
+	        echo "Esse é o site oficial";
+	        break;
+	    case "!he4rt":
+	        echo "Entre no nosso discord: https://discord.com/he4rt";
+	        echo "Esse é o discord oficial";
+	        break;
+	    case "!comandos":
+	        echo "Segue a lista de comandos";
+	        echo "!he4rt !site";
+	        break;
+	    default:
+	        echo "nada acontece feijoada";
+	        break;
+	}
+	// return Nada acontece feijoada
+
+?>
